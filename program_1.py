0c3e206308edd3cb365c76d8e78025e8ef1b11c4
javangraber-design <javangraber@gmail.com>
2026-01-29 19:24:09 -0600

    Javan Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,16 @@
 def personal_information():
-    print('...')
 
+    #Give fictional name
+    print('Harold Martin')
 
-# Line which calls the above function.
+    #Give fictional address with location and ZIP code
+    print('123 Beautiful Lane Road')
+    print('Nashville, TN 55893')
+
+    #Give fictional telephone number
+    print('612-894-7878')
+
+    #Give any college major
+    print('Computer Science and Engineering')
+
 personal_information()
